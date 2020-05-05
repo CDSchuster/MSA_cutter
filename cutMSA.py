7aa216f8e283dba8fcfddde52601708b410f5021
claudio <claudio.schuster.93@gmail.com>
2020-05-05 17:00:08 -0300

    converti translate a clase e hice modificaciones menores a cutMSA.py

--- cutMSA.py
+++ cutMSA.py
@@ -1,9 +1,10 @@
 from Bio import SeqIO
 import argparse
 
-def getQuerySeq(query):
+def getQuerySeq(query, rev):
     """Toma la secuencia de un query y la devuelve como string"""
     q_seq=str(list(SeqIO.parse(query, "fasta"))[0].seq)
+    if rev: q_seq=q_seq[::-1]
     return q_seq
 
 def getPosition(query_seq, ref):
@@ -13,7 +14,7 @@ def getPosition(query_seq, ref):
     query_seq_index=0
     query_seq_length=len(query_seq)
     largo_seqs=len(ref)
-    error, i = -1, 0
+    error, i = 0, 0
     while i<largo_seqs and query_seq_index<query_seq_length:
         if ref[query_seq_index+i]!=query_seq[query_seq_index]:
             error+=1
@@ -27,11 +28,11 @@ def getPosition(query_seq, ref):
     final=i+query_seq_index
     return [i, final]
 
-def cutMSA(MSA, query, outFile):
+def cutMSA(MSA, query, outFile, rev):
     """Recibe el MSA, el archivo salida y el query, ejecuta las demas funciones,
     obtiene las posiciones de alineamiento y recorta el MSA a esas posiciones.
     El resultado lo escribe en la salida"""
-    query_seq=getQuerySeq(query)
+    query_seq=getQuerySeq(query, rev)
     fasta=list(SeqIO.parse(MSA, "fasta"))
     ref=fasta[0].seq
     lims=getPosition(query_seq, ref)
@@ -46,13 +47,14 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-msa", nargs='+')
     parser.add_argument("-q", nargs='+')
+    parser.add_argument("-rev", nargs='+', type=bool)
     parser.add_argument("-o", nargs='+')
     return parser.parse_args()
 
 def main():
     """Funcion donde se ejcutan todas las demas funcionas en serie"""
     args=parse_arguments()
-    cutMSA(args.msa[0], args.q[0], args.o[0])
+    cutMSA(args.msa[0], args.q[0], args.o[0], args.rev[0])
     return 0
 
 if __name__=='__main__':
--- translate.py
+++ translate.py
@@ -3,58 +3,56 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import IUPAC
 
-def loadFasta(fasFile):
-    fasta=list(SeqIO.parse(fasFile, "fasta"))
-    return fasta
+class translator(object):
 
-def translate_seq(seq):
-    seq=seq.upper()
-    table = {
-        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
-        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
-        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
-        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
-        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
-        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
-        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
-        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
-        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
-        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
-        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
-        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
-        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
-        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-        'TAC':'Y', 'TAT':'Y', 'TAA':'*', 'TAG':'*',
-        'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W',
-    }
-    protein =""
-    nts=["A", "T", "C", "G"]
-    if len(seq)%3 == 0:
-        for i in range(0, len(seq), 3):
-            codon = seq[i:i + 3]
-            if codon[0] in nts and codon[1] in nts and codon[2] in nts:
-                protein+= table[codon]
-            else: protein+="X"
-    return protein
+    def __init__(self, inFile, outFile):
+        fasta=self.__loadFasta__(inFile)
+        records=self.__translate_MSA__(fasta)
+        self.__writeFasta__(records, outFile)
 
-def translate_MSA(fasta):
-    records=[]
-    for rec in fasta:
-        name=rec.description.replace(" ", "")
-        aa=Seq(translate_seq(str(rec.seq)), IUPAC.protein)
-        newRec=SeqRecord(aa, id=name, description="")
-        records.append(newRec)
-    return records
+    def __loadFasta__(self, fasFile):
+        fasta=list(SeqIO.parse(fasFile, "fasta"))
+        return fasta
 
-def writeFasta(records, outFile):
-    SeqIO.write(records, outFile, "fasta")
-    return 0
+    def __translate_seq__(self, seq):
+        seq=seq.upper()
+        table = {
+            'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+            'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+            'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+            'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+            'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+            'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+            'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+            'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+            'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+            'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+            'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+            'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+            'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+            'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+            'TAC':'Y', 'TAT':'Y', 'TAA':'*', 'TAG':'*',
+            'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W',
+        }
+        protein =""
+        nts=["A", "T", "C", "G"]
+        if len(seq)%3 == 0:
+            for i in range(0, len(seq), 3):
+                codon = seq[i:i + 3]
+                if codon[0] in nts and codon[1] in nts and codon[2] in nts:
+                    protein+= table[codon]
+                else: protein+="X"
+        return protein
 
-def main(inFile, outFile):
-    fasta=loadFasta(inFile)
-    records=translate_MSA(fasta)
-    writeFasta(records, outFile)
-    return 0
+    def __translate_MSA__(self, fasta):
+        records=[]
+        for rec in fasta:
+            name=rec.description.replace(" ", "")
+            aa=Seq(self.__translate_seq__(str(rec.seq)), IUPAC.protein)
+            newRec=SeqRecord(aa, id=name, description="")
+            records.append(newRec)
+        return records
 
-if __name__=='__main__':
-    main(inFile, outFile)
+    def __writeFasta__(self, records, outFile):
+        SeqIO.write(records, outFile, "fasta")
+        return 0
