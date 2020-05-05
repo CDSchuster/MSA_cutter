08ca3bcba9c940cc119bff1249633632de907d64
claudio <claudio.schuster.93@gmail.com>
2020-05-05 13:02:56 -0300

    Arregle la funcion getPositions para que no explote cuando hay snps

--- cutMSA.py
+++ cutMSA.py
@@ -13,20 +13,19 @@ def getPosition(query_seq, ref):
     query_seq_index=0
     query_seq_length=len(query_seq)
     largo_seqs=len(ref)
-    error=-1
-    for i in range(largo_seqs):
-        if query_seq_index<query_seq_length and ref[i]!=query_seq[query_seq_index]:
-            error=error+1
-        if  query_seq_index<query_seq_length and ref[i]==query_seq[query_seq_index]:
+    error, i = -1, 0
+    while i<largo_seqs and query_seq_index<query_seq_length:
+        if ref[query_seq_index+i]!=query_seq[query_seq_index]:
+            error+=1
             query_seq_index=query_seq_index+1
-        if error>3 or query_seq_index==error:
+        elif ref[query_seq_index+i]==query_seq[query_seq_index]:
+            query_seq_index=query_seq_index+1
+        if error>10 or query_seq_index==error:
             query_seq_index=0
-            error=-1
-        if query_seq_length==query_seq_index:
-            inicio=i-query_seq_length-error
-            final=i+1
-            query_seq_length=0
-    return [inicio, final]
+            error=0
+            i+=1
+    final=i+query_seq_index
+    return [i, final]
 
 def cutMSA(MSA, query, outFile):
     """Recibe el MSA, el archivo salida y el query, ejecuta las demas funciones,
